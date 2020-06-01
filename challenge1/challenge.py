6f00c67c1315441a1a73c3af338d9e49b3e1e1fc
ISENGRIM\caleb <calebdlaguna@hotmail.com>
2020-06-01 11:11:11 -0400

    small changes

--- challenge1/challenge.py
+++ challenge1/challenge.py
@@ -1,2 +1,21 @@
-move = input("Enter move:")
-print("move is: " + move)
+from PyDictionary import PyDictionary
+import array
+import numpy as np
+
+dictionary=PyDictionary()
+
+# would be useful to check if entry is a word or a number
+# if a word get type (ex. noun)
+# if a number perhaps prefix question with "How many... ?"
+
+def returnQuestions(text, numbofquestions):
+    # number questions (question, answer, options)
+    np_questions =  np.zeros((numbofquestions, 3), dtype='<U5')
+    print(np_questions)
+
+
+
+def returnTypeOfWord(word):
+    return "Noun"
+
+returnQuestions("hello, my name is John. I am a tall fellow", 2)
