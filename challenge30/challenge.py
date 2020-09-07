708665084e7a23d8364243fd67965c8b1caea5a2
ISENGRIM\caleb <calebdlaguna@hotmail.com>
2020-09-07 17:26:48 -0500

    3 questions per row

--- challenge30/challenge.py
+++ challenge30/challenge.py
@@ -11,9 +11,17 @@ def generateMathsPdf(name, type, numbofquestions, skillLevel):
     pdf.set_font("Arial", size=12)
     pdf.cell(200, 10, txt=name, ln=1, align="C")
     np_arr = generateAdditionQuestions(numbofquestions, skillLevel)
+    counter = 0
     for x in range(numbofquestions):
-        question =  str(np_arr[x, 0]) + " X " + str(np_arr[x, 1]) + " = "
-        pdf.cell(200, 10, txt=question, ln=1)
+        counter = counter + 1
+
+        question =  str(np_arr[x, 0]) + " * " + str(np_arr[x, 1]) + " = "
+        if (counter % 3 == 0):
+            pdf.cell(30)
+            pdf.cell(20, 10, txt=question, ln=1)
+        else:
+            pdf.cell(30)
+            pdf.cell(20, 10, txt=question, ln=0)
 
     pdf.output(name +".pdf")
 
@@ -25,11 +33,11 @@ def generateAdditionQuestions (numbofquestions, skillLevel):
         4: 70,
         5: 100
     }
-    np_arr  = np.array([random.randint(3, difficulties.get(skillLevel,10)), random.randint(3, difficulties.get(skillLevel,10))])
+    np_arr  = np.array([random.randint(11, difficulties.get(skillLevel,10)), random.randint(11, difficulties.get(skillLevel,10))])
 
     for x in range(numbofquestions):
-        np_arr2 = np.array([random.randint(3, difficulties.get(skillLevel,10)), random.randint(3, difficulties.get(skillLevel,10))])
+        np_arr2 = np.array([random.randint(11, difficulties.get(skillLevel,10)), random.randint(11, difficulties.get(skillLevel,10))])
         np_arr = np.vstack((np_arr,np_arr2))
     return np_arr
 
-generateMathsPdf("Simple Addition", "choice", 10, 3);
+generateMathsPdf("addition6", "choice", 42, 3);
