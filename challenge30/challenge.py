2a1d40bd193acacdbcaa2839567bdc4ca3efbefa
ISENGRIM\caleb <calebdlaguna@hotmail.com>
2020-09-21 16:36:56 -0500

    allow for bigger numbers and more questions

--- challenge30/challenge.py
+++ challenge30/challenge.py
@@ -2,28 +2,8 @@ from fpdf import FPDF
 import array
 import numpy as np
 import random
-#
-
-def generateMathsPdf(name, type, numbofquestions, skillLevel):
-    # generate a quiz
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=12)
-    pdf.cell(200, 10, txt=name, ln=1, align="C")
-    np_arr = generateNumberPairs(numbofquestions, skillLevel)
-    counter = 0
-    for x in range(numbofquestions):
-        counter = counter + 1
-
-        question =  str(np_arr[x, 0]) + " * " + str(np_arr[x, 1]) + " = "
-        if (counter % 3 == 0):
-            pdf.cell(30)
-            pdf.cell(20, 10, txt=question, ln=1)
-        else:
-            pdf.cell(30)
-            pdf.cell(20, 10, txt=question, ln=0)
-
-    pdf.output(name +".pdf")
+from numpy import *
+# modified to allow for bigger numbers and more questions
 
 def springMix(name, numbofquestions, skillLevel):
 # produces math quiz with addition, subtraction, multiplication, and division
@@ -35,18 +15,20 @@ def springMix(name, numbofquestions, skillLevel):
         counter = counter + 1
 
         question =  str(np_arr[x, 0]) + returnOperator(10, 10, 50, 30, x, numbofquestions) + str(np_arr[x, 1]) + " = "
-        if (counter % 3 == 0):
-            pdf.cell(30)
-            pdf.cell(20, 10, txt=question, ln=1)
+        if (counter % 5 == 0):
+            pdf.cell(20)
+            pdf.cell(10, 5, txt=question, ln=1)
         else:
-            pdf.cell(30)
-            pdf.cell(20, 10, txt=question, ln=0)
+            pdf.cell(20)
+            pdf.cell(10, 5, txt=question, ln=0)
 
     pdf.output(name +".pdf")
 
 
 def returnOperator(additionstop, subtractionstop, multiplicationstop, divisionstop, current, total):
     # return a different operator based on the current spot
+    # potential operators +, -, /, %
+    # potential questions involving: factorial, square root, square (power)
     operands = {
         1: " + ",
         2: " - ",
@@ -78,15 +60,17 @@ def generateGeneralPdf(name):
     pdf.cell(200, 10, txt=name, ln=1, align="C")
     return pdf
 
+
+def generateDifficulties(len):
+    difficulties = {}
+    for x in arange(len):
+        difficulties[x] = x * 10
+    return difficulties
+
 def generateNumberPairs (numbofquestions, skillLevel):
     # the higher the difficulty the bigger the number
-    difficulties = {
-        1: 10,
-        2: 20,
-        3: 40,
-        4: 70,
-        5: 100
-    }
+    difficulties = generateDifficulties(10)
+
     np_arr  = np.array([random.randint(11, difficulties.get(skillLevel,10)), random.randint(11, difficulties.get(skillLevel,10))])
 
     for x in range(numbofquestions):
@@ -94,5 +78,4 @@ def generateNumberPairs (numbofquestions, skillLevel):
         np_arr = np.vstack((np_arr,np_arr2))
     return np_arr
 
-# generateMathsPdf("addition6", "choice", 42, 3);
-springMix("test 5", 69, 3)
+springMix("test 6", 150, 9)
